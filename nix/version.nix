a37a0d6b0db9385d8478bcc0fd5fee948e75578a
Link <linkfrg.dev@proton.me>
2025-07-31 01:51:04 +0500

    nix: remove `post1.dev0` from version (they're useless because they are hard-coded)

--- nix/version.nix
+++ nix/version.nix
@@ -2,4 +2,4 @@
   version = "0.5"; # FIXME: hard-coded
   rev = self.shortRev or "dirty";
   date = builtins.substring 0 8 (self.lastModifiedDate or "19700101");
-in "${version}.post1.dev0+g${rev}.d${date}"
+in "${version}+g${rev}.d${date}"
